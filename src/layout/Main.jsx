8494f62ab4de30a1924e96cd1cf05b6eb36f8639
FerdousFardin <fardinh39u@gmaile.com>
2022-11-24 00:39:23 +0600

    navbar, react router and firebase added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../pages/shared/Navbar";
 
 export default function Main() {
   return (
     <>
-      <nav />
+      <Navbar />
       <Outlet />
       <footer />
     </>
