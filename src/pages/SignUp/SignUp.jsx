6306a1c898d20fa2dfdd21a71cf483b4889db781
FerdousFardin <fardinh39u@gmaile.com>
2022-11-25 14:39:57 +0600

    sign up functionality added

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,13 +1,75 @@
+import axios from "axios";
 import React, { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthProvider";
 
 export default function SignUp() {
-  const { singupUser, loading, setLoading } = useContext(AuthContext);
+  const { signupUser, loading, setLoading, googleLogin } =
+    useContext(AuthContext);
+  const [signUpErrors, setSignUpErrors] = useState("");
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
   const [accountType, setAccountType] = useState("buyer");
   const activeClass =
     "flex justify-center w-full px-6 py-3 text-white bg-primary rounded-md md:w-auto md:mx-2 <focus:outline-none></focus:outline-none>";
   const inActiveClass =
     "flex justify-center w-full px-6 py-3 mt-4 text-primary border border-primary rounded-md md:mt-0 md:w-auto md:mx-2 dark:border-primary dark:text-primary/25 focus:outline-none";
+
+  const handleSignup = (data, e) => {
+    setSignUpErrors("");
+    const { password, confirmPassword, email } = data;
+    const formData = new FormData();
+    formData.append("image", data.image[0]);
+    if (password !== confirmPassword) {
+      setSignUpErrors("Password doesn't match.");
+      return;
+    }
+
+    signupUser(email, password)
+      .then((res) => {
+        console.log(res.user);
+        fetch(
+          `https://api.imgbb.com/1/upload?key=${
+            import.meta.env.VITE_imbb_apikey
+          }`,
+          {
+            method: "POST",
+            body: formData,
+          }
+        )
+          .then((res) => res.json())
+          .then(({ data: { url } }) => {
+            // console.log(url);
+            if (url) {
+              const userInfo = {
+                photoURL: url,
+                email,
+                name: `${data.firstName} ${data.lastName}`,
+                role: [accountType],
+              };
+              axios
+                .post(`${import.meta.env.VITE_API_URL}/users`, userInfo)
+                .then((res) => {
+                  console.log(res);
+
+                  if (res.data.acknowledged) {
+                    e.reset();
+                  }
+                });
+            }
+          });
+      })
+      .catch((er) => {
+        console.error(er);
+        setSignUpErrors(er.code);
+        setLoading(false);
+      });
+  };
+
   return (
     <section class="bg-white dark:bg-gray-900">
       <div class="flex justify-center min-h-screen">
@@ -81,12 +143,18 @@ export default function SignUp() {
               </div>
             </div>
 
-            <form class="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
+            <form
+              onSubmit={handleSubmit(handleSignup)}
+              class="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2"
+            >
               <div>
                 <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">
                   First Name
                 </label>
                 <input
+                  {...register("firstName", {
+                    required: true,
+                  })}
                   type="text"
                   placeholder="John"
                   class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-primary dark:focus:border-primary focus:ring-primary focus:outline-none focus:ring focus:ring-opacity-40"
@@ -98,6 +166,9 @@ export default function SignUp() {
                   Last name
                 </label>
                 <input
+                  {...register("lastName", {
+                    required: true,
+                  })}
                   type="text"
                   placeholder="Snow"
                   class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-primary dark:focus:border-primary focus:ring-primary focus:outline-none focus:ring focus:ring-opacity-40"
@@ -105,14 +176,39 @@ export default function SignUp() {
               </div>
 
               <div>
-                <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-                  Phone number
+                <p class="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                  Upload Profile Picture
+                </p>
+                <label
+                  for="dropzone-file"
+                  class="flex  items-center w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-primary dark:focus:border-primary focus:ring-primary focus:outline-none focus:ring focus:ring-opacity-40"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="w-6 h-6 text-gray-300 dark:text-gray-500"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
+                    />
+                  </svg>
+
+                  <h2 class="mx-3 text-gray-400">Profile Photo</h2>
+
+                  <input
+                    {...register("image", {
+                      required: true,
+                    })}
+                    id="dropzone-file"
+                    type="file"
+                    class="invisible h-0 w-0"
+                  />
                 </label>
-                <input
-                  type="text"
-                  placeholder="XXX-XX-XXXX-XXX"
-                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-primary dark:focus:border-primary focus:ring-primary focus:outline-none focus:ring focus:ring-opacity-40"
-                />
               </div>
 
               <div>
@@ -120,6 +216,9 @@ export default function SignUp() {
                   Email address
                 </label>
                 <input
+                  {...register("email", {
+                    required: true,
+                  })}
                   type="email"
                   placeholder="johnsnow@example.com"
                   class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-primary dark:focus:border-primary focus:ring-primary focus:outline-none focus:ring focus:ring-opacity-40"
@@ -131,6 +230,9 @@ export default function SignUp() {
                   Password
                 </label>
                 <input
+                  {...register("password", {
+                    required: true,
+                  })}
                   type="password"
                   placeholder="Enter your password"
                   class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-primary dark:focus:border-primary focus:ring-primary focus:outline-none focus:ring focus:ring-opacity-40"
@@ -142,6 +244,9 @@ export default function SignUp() {
                   Confirm password
                 </label>
                 <input
+                  {...register("confirmPassword", {
+                    required: true,
+                  })}
                   type="password"
                   placeholder="Enter your password"
                   class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-primary dark:focus:border-primary focus:ring-primary focus:outline-none focus:ring focus:ring-opacity-40"
@@ -150,10 +255,11 @@ export default function SignUp() {
 
               <div>
                 <button
+                  disabled={loading}
                   type="submit"
-                  class="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-black rounded-md hover:bg-primary focus:outline-none focus:ring focus:ring-primary-100/20 focus:ring-opacity-50"
+                  class="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-black rounded-md disabled:cursor-not-allowed disabled:bg-gray-400 hover:bg-primary focus:outline-none focus:ring focus:ring-primary-100/20 focus:ring-opacity-50"
                 >
-                  <span>Sign Up </span>
+                  <span>{loading ? "Signing Up..." : "Sign Up"}</span>
 
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -169,34 +275,50 @@ export default function SignUp() {
                   </svg>
                 </button>
               </div>
-              <div>
-                <a
-                  href="#"
-                  class="flex items-center justify-center px-6 py-3 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
-                >
-                  <svg class="w-6 h-6 mx-2" viewBox="0 0 40 40">
-                    <path
-                      d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
-                      fill="#FFC107"
-                    />
-                    <path
-                      d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
-                      fill="#FF3D00"
-                    />
-                    <path
-                      d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
-                      fill="#4CAF50"
-                    />
-                    <path
-                      d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
-                      fill="#1976D2"
-                    />
-                  </svg>
-
-                  <span class="mx-2">Sign in with Google</span>
-                </a>
-              </div>
             </form>
+            <div className="w-1/2 text-center my-5">
+              <a
+                href="#"
+                class="text-xs text-center text-gray-500 uppercase dark:text-gray-400 hover:underline"
+              >
+                or login with Social Media
+              </a>
+            </div>
+            <div className="w-1/2">
+              <a
+                href="#"
+                class="flex items-center justify-center mt-5 px-6 py-3 text-gray-600 transition-colors duration-300 transform bg-gray-100 border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-600"
+              >
+                <svg class="w-6 h-6 mx-2" viewBox="0 0 40 40">
+                  <path
+                    d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+                    fill="#FFC107"
+                  />
+                  <path
+                    d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
+                    fill="#FF3D00"
+                  />
+                  <path
+                    d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
+                    fill="#4CAF50"
+                  />
+                  <path
+                    d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+                    fill="#1976D2"
+                  />
+                </svg>
+
+                <span class="mx-2">Sign in with Google</span>
+              </a>
+            </div>
+            <div className="w-1/2 text-gray-700 mt-5">
+              Already have an account?{" "}
+              <Link to={"/login"} class="-ml-3 w-max p-3">
+                <span class="text-sm tracking-wide text-primary dark:text-primary">
+                  Sign In here
+                </span>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
